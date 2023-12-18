9668c73979a9714bced5bd4383dfe76d5c176e3e
Rilary123 <131264147+Rilary123@users.noreply.github.com>
2023-12-18 12:16:48 -0300

    Update login.js

--- projeto final/login.js
+++ projeto final/login.js
@@ -1,6 +1,5 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    // Aqui você pode adicionar lógica para verificar o login e redirecionar para a página de lista
     window.location.href = 'lista.html';
   });
   
